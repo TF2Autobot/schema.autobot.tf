0b904b33a23a47db6034398bbe5449113198474c
IdiNium <47635037+idinium96@users.noreply.github.com>
2022-06-27 09:35:49 +0800

    🔨 refactor fastify

--- src/app.ts
+++ src/app.ts
@@ -6,91 +6,14 @@ import dotenv from 'dotenv';
 import log, { init } from './lib/logger';
 import SchemaManager from './schemaManager';
 import fastify from 'fastify';
-
-import schema from './routes/schema/index';
-import properties from './routes/properties/index';
-import getName from './routes/getName/index';
-import getSku from './routes/getSku/index';
-import getItemObject from './routes/getItemObject/index';
-import getItem from './routes/getItem/index';
-import raw from './routes/raw/index';
+import fastifySetup from './fastify';
 
 const paths = genPaths();
 init(paths);
 dotenv.config({ path: paths.env });
 
 const server = fastify({ keepAliveTimeout: 200 });
-void import('@fastify/swagger').then(async sw => {
-    // @ts-ignore
-    server.register(sw, {
-        exposeRoute: true,
-        routePrefix: '/',
-        openapi: {
-            info: {
-                title: 'Team Fortress 2 Schema Public Unofficial APIs',
-                description: 'Documentation for schema.autobot.tf public APIs.',
-                version: process.env.SERVER_VERSION,
-                license: 'MIT'
-            },
-            externalDocs: {
-                url: 'https://github.com/TF2Autobot/schema.autobot.tf',
-                description: 'Github repository'
-            }
-        }
-    });
-
-    log.debug('Initiaziling schema manager...');
-    try {
-        await SchemaManager.init();
-    } catch (err) {
-        throw err;
-    }
-
-    log.debug('Initiaziling hooks...');
-    // https://stackoverflow.com/questions/57645360/fastify-middleware-access-to-query-and-params
-    server.addHook('onRequest', (req, res, done) => {
-        if (['/static', '/uiConfig', '/json', '/initOAuth'].some(key => req.url.includes(key))) {
-            return done();
-        }
-
-        log.info(`Got ${req.method} ${decodeURIComponent(req.url)}`);
-
-        done();
-    });
-
-    log.debug('Initiaziling routes...');
-    server.register(schema, {
-        prefix: '/schema'
-    });
-    server.register(properties, {
-        prefix: '/properties'
-    });
-    server.register(getName, {
-        prefix: '/getName'
-    });
-    server.register(getSku, {
-        prefix: '/getSku'
-    });
-    server.register(getItemObject, {
-        prefix: '/getItemObject'
-    });
-    server.register(getItem, {
-        prefix: '/getItem'
-    });
-    server.register(raw, {
-        prefix: '/raw'
-    });
-
-    const port = parseInt(process.env.PORT, 10);
-
-    log.debug('Starting the server...');
-    try {
-        await server.listen({ port });
-        log.debug(`Server is up! Listening at http://localhost:${port}`);
-    } catch (err) {
-        throw err;
-    }
-});
+fastifySetup(server);
 
 import ON_DEATH from 'death';
 import { inspect } from 'util';
@@ -116,5 +39,6 @@ ON_DEATH({ uncaughtException: true })((signalOrErr, origin) => {
     log.info('Server uptime:' + process.uptime());
     clearInterval(SchemaManager?.schemaManager?._updateInterval);
     clearTimeout(SchemaManager?.schemaManager?._updateTimeout);
+    server?.close();
     process.exit(1);
 });
---
+++ src/fastify.ts
@@ -0,0 +1,83 @@
+import SchemaManager from './schemaManager';
+import log from './lib/logger';
+import fastifySwagger from '@fastify/swagger';
+import { FastifyInstance } from 'fastify';
+
+import schema from './routes/schema/index';
+import properties from './routes/properties/index';
+import getName from './routes/getName/index';
+import getSku from './routes/getSku/index';
+import getItemObject from './routes/getItemObject/index';
+import getItem from './routes/getItem/index';
+import raw from './routes/raw/index';
+
+export default async function fastifySetup(server: FastifyInstance): Promise<void> {
+    // @ts-ignore
+    server.register(fastifySwagger, {
+        exposeRoute: true,
+        routePrefix: '/',
+        openapi: {
+            info: {
+                title: 'Team Fortress 2 Schema Public Unofficial APIs',
+                description: 'Documentation for schema.autobot.tf public APIs.',
+                version: process.env.SERVER_VERSION
+            },
+            externalDocs: {
+                url: 'https://github.com/TF2Autobot/schema.autobot.tf',
+                description: 'Github repository'
+            }
+        }
+    });
+
+    log.debug('Initiaziling schema manager...');
+    try {
+        await SchemaManager.init();
+    } catch (err) {
+        throw err;
+    }
+
+    log.debug('Initiaziling hooks...');
+    // https://stackoverflow.com/questions/57645360/fastify-middleware-access-to-query-and-params
+    server.addHook('onRequest', (req, res, done) => {
+        if (['/static', '/uiConfig', '/json', '/initOAuth'].some(key => req.url.includes(key))) {
+            return done();
+        }
+
+        log.info(`Got ${req.method} ${decodeURIComponent(req.url)}`);
+
+        done();
+    });
+
+    log.debug('Initiaziling routes...');
+    server.register(schema, {
+        prefix: '/schema'
+    });
+    server.register(properties, {
+        prefix: '/properties'
+    });
+    server.register(getName, {
+        prefix: '/getName'
+    });
+    server.register(getSku, {
+        prefix: '/getSku'
+    });
+    server.register(getItemObject, {
+        prefix: '/getItemObject'
+    });
+    server.register(getItem, {
+        prefix: '/getItem'
+    });
+    server.register(raw, {
+        prefix: '/raw'
+    });
+
+    const port = parseInt(process.env.PORT, 10);
+
+    log.debug('Starting the server...');
+    try {
+        await server.listen({ port });
+        log.debug(`Server is up! Listening at http://localhost:${port}`);
+    } catch (err) {
+        throw err;
+    }
+}
