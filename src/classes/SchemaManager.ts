cc5aaed16d300d6cbb978d1c5184670187fe9062
idinium96 <47635037+idinium96@users.noreply.github.com>
2024-07-19 04:00:51 +0800

    🔨 increase sleep time for sending webhooks

--- src/classes/SchemaManager.ts
+++ src/classes/SchemaManager.ts
@@ -374,7 +374,7 @@ type WebhookType = 'items' | 'effects' | 'paintkits' | 'skus';
 class WebhookQueue {
     private static webhooks: { url: string; type: WebhookType; webhook: Webhook }[] = [];
 
-    private static sleepTime = 1000;
+    private static sleepTime = 5000;
 
     private static isRateLimited = false;
 
@@ -408,7 +408,7 @@ class WebhookQueue {
         await timersPromises.setTimeout(this.sleepTime);
 
         if (this.isRateLimited) {
-            this.sleepTime = 1000;
+            this.sleepTime = 5000;
             this.isRateLimited = false;
         }
 
@@ -425,8 +425,8 @@ class WebhookQueue {
             .catch(err => {
                 log.warn(`Error sending webhook on new ${webhook.type} update`, filterAxiosError(err));
 
-                if (typeof err.data !== 'string' && err.data?.message === 'The resource is being rate limited.') {
-                    this.sleepTime = err.data.retry_after;
+                if (err.status === 429) {
+                    this.sleepTime = 10000;
                     this.isRateLimited = true;
 
                     this.isProcessing = false;
