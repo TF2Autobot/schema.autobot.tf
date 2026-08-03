ce3caa869c31cb667380a6ea11b928450951c7f9
idinium96 <47635037+idinium96@users.noreply.github.com>
2026-08-04 03:37:41 +0800

    ✨ add two new endpoints that were added on the latest @tf2autobot/tf2-schema

--- src/routes/raw/index.ts
+++ src/routes/raw/index.ts
@@ -95,6 +95,90 @@ const raw: FastifyPluginAsync = async (app: FastifyInstance, opts?: RegisterOpti
             });
         }
     );
+
+    app.get(
+        '/isFestivizable/:defindex',
+        {
+            schema: {
+                description: 'Determine if the defindex can be festivized, return a boolean',
+                tags: ['Schema (raw)'],
+                params: {
+                    type: 'object',
+                    properties: {
+                        defindex: {
+                            type: 'number',
+                            description: `Example (true): 35, 36, 127, 423, 1178, 15000,`
+                        }
+                    }
+                }
+            }
+        },
+        (req, reply) => {
+            // @ts-ignore
+            if (req.params?.defindex === undefined) {
+                return reply.code(400).header('Content-Type', 'application/json; charset=utf-8').send({
+                    success: false,
+                    message: 'params of "defindex" MUST be defined'
+                });
+            }
+
+            // @ts-ignore
+            const defindex = parseInt(req.params.defindex) as number;
+            const isFestivizable = SchemaManager.schemaManager.schema.isFestivizable(defindex);
+
+            if (typeof isFestivizable !== 'boolean') {
+                return reply
+                    .code(404)
+                    .header('Content-Type', 'application/json; charset=utf-8')
+                    .send({
+                        success: false,
+                        message: `isFestivizable return ${isFestivizable} for defindex ${defindex}`
+                    });
+            }
+
+            return reply.code(200).header('Content-Type', 'application/json; charset=utf-8').send({
+                success: true,
+                isFestivizable
+            });
+        }
+    );
+
+    app.get(
+        '/getStrangifierTarget/:defindex',
+        {
+            schema: {
+                description: 'Get Strangifier target (td-), return number or null',
+                tags: ['Schema (raw)'],
+                params: {
+                    type: 'object',
+                    properties: {
+                        defindex: {
+                            type: 'number',
+                            description: `Example (with valid value): 5661, 5721, 5722, 5723, 5724`
+                        }
+                    }
+                }
+            }
+        },
+        (req, reply) => {
+            // @ts-ignore
+            if (req.params?.defindex === undefined) {
+                return reply.code(400).header('Content-Type', 'application/json; charset=utf-8').send({
+                    success: false,
+                    message: 'params of "defindex" MUST be defined'
+                });
+            }
+
+            // @ts-ignore
+            const defindex = parseInt(req.params.defindex) as number;
+            const strangifierTarget = SchemaManager.schemaManager.schema.getStrangifierTarget(defindex);
+
+            return reply.code(200).header('Content-Type', 'application/json; charset=utf-8').send({
+                success: true,
+                strangifierTarget
+            });
+        }
+    );
 };
 
 export default raw;
